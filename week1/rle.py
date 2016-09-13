78ad604d78bb1bf6ef7ce3afd2e74fed4fb89f69
MathuraMG <mmg9291@gmail.com>
2016-09-13 19:17:53 -0400

    add decoder

--- week1/rle.py
+++ week1/rle.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+global_output = []
+
 def encode(input):
     prev_char = input[0]
     curr_char = input[1]
@@ -14,15 +16,28 @@ def encode(input):
             output.append([curr_char,1])
         prev_char = curr_char
 
+    print('The encoded output is - ',end='')
     for i in range(len(output)):
         print(output[i][1],end='')
         print(output[i][0],end='')
-    print('');
+    print('\n\n********************************************\ndecoding\n\n')
 
+    input = ''
+    for i in range(len(output)):
+        for j in range(output[i][1]):
+            input+=(output[i][0])
+    print('The decoded input is - ',end='')
+    print(input)
 
 def main():
-    test = raw_input("input : ")
-    encode(test)
+    while True:
+        print('\n\n********************************************\ntype exit to leave program\n\n')
+        input_string = raw_input("enter input : ")
+        if(input_string == 'exit'):
+            break
+        print('\n\n')
+        encode(input_string)
+
 
 if __name__ == "__main__":
     main()

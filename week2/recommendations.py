f129b4db0c851a696fb515146a1a8cd5b6e709eb
MathuraMG <mmg9291@gmail.com>
2016-09-24 20:15:04 -0400

    add getting recommendations

--- week2/recommendations.py
+++ week2/recommendations.py
@@ -6,7 +6,7 @@ class recommendation():
     'mathura':{'CCD':2,'Kaapi':5,'Joes':3,'Starbucks':1},
     'madhu':{'CCD':1,'Kaapi':2,'Joes':4},
     'shroom':{'CCD':3,'Joes':5,'Starbucks':4},
-    'sam':{'Kaapi':1,'Joes':2,'Starbucks':5},
+    'sam':{'Kaapi':1,'Joes':2},
     'mathur':{'Kaapi':1,'Joes':2,'Starbucks':5}
     }
 
@@ -29,5 +29,25 @@ class recommendation():
         scores.reverse();
         print scores
 
+    def getReco(self,person_interest):
+        totals = {}
+        no_review = {}
+        ranking = {}
+        for person in self.people:
+            if person!=person_interest:
+                score = self.compare(person_interest,person)
+                for item in self.people[person]:
+                    if item not in self.people[person_interest]:
+                        totals.setdefault(item,0)
+                        no_review.setdefault(item,0)
+                        totals[item]+=score*self.people[person][item]
+                        no_review[item]+=score
+
+        for item  in totals:
+            ranking[item] = totals[item]/no_review[item]
+        print totals
+        print no_review
+        print ranking
 
 # from recommendations import recommendation
+# reco = recommendation()

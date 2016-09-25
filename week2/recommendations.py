32ebdb53e98b18cdf4896cfb7b21e70d81c64cbf
MathuraMG <mmg9291@gmail.com>
2016-09-24 21:00:18 -0400

    add similar item

--- week2/recommendations.py
+++ week2/recommendations.py
@@ -3,28 +3,36 @@ from math import sqrt
 
 class recommendation():
     people={
-    'mathura':{'CCD':2,'Kaapi':5,'Joes':3,'Starbucks':1},
-    'madhu':{'CCD':1,'Kaapi':2,'Joes':4},
-    'shroom':{'CCD':3,'Joes':5,'Starbucks':4},
-    'sam':{'Kaapi':1,'Joes':2},
-    'mathur':{'Kaapi':1,'Joes':2,'Starbucks':5}
+    'mathura':{'CCD':2,'Kaapi':5,'Joes':5,'Starbucks':1},
+    'madhu':{'CCD':1,'Kaapi':5,'Joes':5},
+    'shroom':{'CCD':3,'Kaapi':5,'Joes':5,'Starbucks':4},
+    'sam':{'Kaapi':5,'Joes':5},
+    'mathur':{'Kaapi':5,'Joes':5,'Starbucks':5}
     }
 
     def test(self):
         print 'hello'
 
-    def compare(self,person1,person2):
+    def transform(self):
+        result = {}
+        for person in self.people:
+            for item in self.people[person]:
+                result.setdefault(item,{})
+                result[item][person] = self.people[person][item]
+        return result
+
+    def compare(self,data,person1,person2):
         sum = 0
-        for item in self.people[person1]:
-            if item in self.people[person2]:
-                sum+=pow((self.people[person1][item]-self.people[person2][item]),2)
+        for item in data[person1]:
+            if item in data[person2]:
+                sum+=pow((data[person1][item]-data[person2][item]),2)
         return 1/sqrt(1+sum)
 
-    def topMatches(self,person_interest):
+    def topMatches(self,data,person_interest):
         scores = []
-        for person in self.people:
+        for person in data:
             if person!=person_interest:
-                scores.append((self.compare(person_interest,person),person))
+                scores.append((self.compare(data,person_interest,person),person))
         scores.sort()
         scores.reverse()
         print scores
@@ -48,6 +56,10 @@ class recommendation():
         ranking.sort()
         ranking.reverse()
         print ranking
+
+    def getSimilarItem(self,item_interest):
+        result = self.transform()
+        self.topMatches(result,item_interest)
 
 # from recommendations import recommendation
 # reco = recommendation()

9d29b168721cd4f5fce26878e232b4917c441a6b
mathuramg <mmg9291@gmail.com>
2016-10-24 08:25:38 -0400

    make MLP generic

--- week4/mlp1.py
+++ week4/mlp1.py
@@ -18,25 +18,90 @@ def tanh_fn(x):
 def tanh_dfn(x):
 	return 1.0 - np.power( tanh_fn( x ), 2.0 )
 
-def feed_forward(l_weights, prev_l_output, l_bias) :
-    l_activations = np.dot( l_weights, prev_l_output ) + l_bias
-    l_output = activation_fn( l_activations )
-    return l_output, l_activations
+class mlp():
 
-def back_propogate(l_activations,l_weights,l_delta,is_last_layer) :
-    if is_last_layer:
-        return activation_dfn(l_activations)*(l_weights-l_delta)
-    else:
-        return activation_dfn(l_activations)*np.dot(l_weights.T,l_delta)
+    sample_size = 1
+    hidden_size = 15
+    # hidden1_size = 15
+    output_size = 1
+    error = []
 
-def apply_delta(l_deltas,l_outputs,l_weights,l_bias):
-    l_weights -= learning_rate * np.dot( l_deltas, l_outputs.T )
-    l_bias -= learning_rate * l_deltas
-    return l_weights, l_bias
+    layer_weights = []
+    layer_bias = []
+    l_activations = []
+    l_outputs = []
+    l_deltas = []
+    epoch = 1
+    no_layers =1
+    layer_size = []
 
+    def init_deltas(self):
+        for i in range(self.no_layers -1):
+            self.l_deltas.append(1)
 
+    def init_bias_weight(self,no_layers):
+        # layer_size = [self.sample_size, self.hidden_size, self.output_size]
+        for i in range(no_layers-1):
+            print i
+            self.layer_weights.append(np.random.rand( self.layer_size[i+1], self.layer_size[i] ))
+            self.layer_bias.append(np.random.rand(self.layer_size[i+1],1))
 
+    def init(self,user_epoch,no_layers,layer_size):
+        self.epoch = user_epoch
+        self.no_layers = no_layers
+        self.layer_size = layer_size
+        self.init_bias_weight(self.no_layers)
+        self.init_deltas()
+        self.error = np.zeros( ( self.sample_size, 1 ) )
 
+    def feed_forward(self,l_weights, prev_l_output, l_bias) :
+        l_activations = np.dot( l_weights, prev_l_output ) + l_bias
+        l_output = activation_fn( l_activations )
+        return l_output, l_activations
+
+    def back_propogate(self,l_activations,l_weights,l_delta,is_last_layer) :
+        if is_last_layer:
+            return activation_dfn(l_activations)*(l_weights-l_delta)
+        else:
+            return activation_dfn(l_activations)*np.dot(l_weights.T,l_delta)
+
+    def apply_delta(self,l_deltas,l_outputs,l_weights,l_bias):
+        l_weights -= learning_rate * np.dot( l_deltas, l_outputs.T )
+        l_bias -= learning_rate * l_deltas
+        return l_weights, l_bias
+
+    def single_loop(self, sample_vec, output_vec):
+        #feed forwards
+        self.l_activations = []
+        self.l_outputs = []
+        ip = sample_vec
+        self.l_outputs.append(ip)
+        for i in range(self.no_layers-1):
+            l_op, l_act = self.feed_forward(self.layer_weights[i],self.l_outputs[i],self.layer_bias[i])
+            self.l_outputs.append(l_op)
+            self.l_activations.append(l_act)
+
+        #back propogation
+        for i in range(self.no_layers-1):
+            j = self.no_layers - 1 - i
+            if i==0:
+                # print self.l_deltas
+                self.l_deltas[j-1] = self.back_propogate(self.l_activations[j-1], self.l_outputs[j], output_vec, True)
+            else:
+                self.l_deltas[j-1] = self.back_propogate(self.l_activations[j-1],self.layer_weights[j],self.l_deltas[j],False)
+
+        #apply deltas
+        for i in range(self.no_layers-1):
+            self.layer_weights[i], self.layer_bias[i]  = self.apply_delta(self.l_deltas[i],self.l_outputs[i], self.layer_weights[i],self.layer_bias[i])
+
+        #error calc
+        self.error +=  np.absolute( output_vec - self.l_outputs[self.no_layers-1] )
+
+    def error_report(self,curr_epoch,reportFreq):
+        # Report error rate:
+        if epoch % reportFreq == 0:
+        	print( "Epoch: %d\nError: %f" % ( curr_epoch, np.sum( self.error ) / float( self.sample_size ) / float( reportFreq ) ) )
+        	self.error = np.zeros( ( self.sample_size, 1 ) )
 
 activation_fn  = tanh_fn
 activation_dfn = tanh_dfn
@@ -46,79 +111,18 @@ reportFreq = 10000
 
 learning_rate = 0.01
 
-sample_size = 1
-hidden_size = 15
-# hidden1_size = 15
-output_size = 1
+# Initialize mlp:
+mlp1 = mlp()
+error = np.zeros( ( mlp1.sample_size, 1 ) )
+mlp1.init(1e6,4,[1,15,15,1])
 
-layer_weights = []
-layer_size = [sample_size, hidden_size, output_size]
-def init_bias_weight(no_layers):
-    for i in range(no_layers-1):
-        print i
-        layer_weights.append(np.random.rand( layer_size[i+1], layer_size[i] ))
+while epoch <= mlp1.epoch :
 
-error = np.zeros( ( sample_size, 1 ) )
-
-# Initialize weights and biases:
-
-init_bias_weight(3)
-# layer0_weights = np.random.rand( hidden_size, sample_size )
-layer0_bias = np.random.rand( hidden_size, 1 )
-
-# layer1_weights = np.random.rand( output_size, hidden_size )
-layer1_bias = np.random.rand( output_size, 1 )
-
-# layer_weights = [layer0_weights, layer1_weights]
-
-# layer2_weights = np.random.rand( output_size, hidden1_size )
-# layer2_bias = np.random.rand( output_size, 1 )
-
-# Perform each epoch:
-while epoch <= 1e6 :
-    # hello('my space');
-    # Choose a random input:
-    sample_vec = np.random.uniform( 0.0, np.pi * 2.0, ( sample_size, 1 ) )
+    sample_vec = np.random.uniform( 0.0, np.pi * 2.0, ( mlp1.sample_size, 1 ) )
     # Compute output:
     output_vec = np.sin( sample_vec )
 
-    # Feed forward (input to hidden):
-    layer1_outputs, layer1_activations = feed_forward(layer_weights[0],sample_vec,layer0_bias)
-
-    # Feed forward (hidden to output):
-    layer2_outputs, layer2_activations = feed_forward(layer_weights[1],layer1_outputs,layer1_bias)
-
-    # Back propagate (output to hidden):
-    # layer2_deltas = activation_dfn( layer2_activations ) * ( layer2_outputs - output_vec )
-    layer2_deltas = back_propogate(layer2_activations,layer2_outputs,output_vec,True)
-
-    # Back propagate (hidden to input):
-    # layer1_deltas = activation_dfn( layer1_activations ) * np.dot( layer_weights[1].T, layer2_deltas )
-    layer1_deltas = back_propogate(layer1_activations,layer_weights[1],layer2_deltas,False)
-
-    # Apply deltas (layer 0):
-    # layer_weights[0] -= learning_rate * np.dot( layer1_deltas, sample_vec.T )
-    # layer0_bias -= learning_rate * layer1_deltas
-    layer_weights[0], layer0_bias   = apply_delta(layer1_deltas,sample_vec, layer_weights[0],layer0_bias)
-
-    # Apply deltas (layer 1):
-    # layer_weights[1] -= learning_rate * np.dot( layer2_deltas, layer1_outputs.T )
-    # layer1_bias -= learning_rate * layer2_deltas
-    layer_weights[1], layer1_bias = apply_delta(layer2_deltas,layer1_outputs, layer_weights[1],layer1_bias)
-
-    # Apply deltas (layer 1):
-    # layer2_weights -= learning_rate * np.dot( layer3_deltas, layer2_outputs.T )
-    # layer2_bias -= learning_rate * layer3_deltas
-
-    # Compute error:
-    error += np.absolute( output_vec - layer2_outputs )
-
-    # Report error rate:
-    if epoch % reportFreq == 0:
-    	# print(layer_weights[1]);
-    	# print(layer_weights[0]);
-    	print( "Epoch: %d\nError: %f" % ( epoch, np.sum( error ) / float( sample_size ) / float( reportFreq ) ) )
-    	error = np.zeros( ( sample_size, 1 ) )
-
+    mlp1.single_loop(sample_vec,output_vec)
+    mlp1.error_report(epoch,reportFreq)
     # Advance epoch iterator:
     epoch += 1
